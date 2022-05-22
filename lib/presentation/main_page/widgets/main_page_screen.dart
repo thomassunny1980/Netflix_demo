0e6c8bb019e56c9f0c0af60154f8ffe3f812d75a
Thomas P Sunny <thomaspsunny@gmail.com>
2022-05-22 22:55:48 +0530

    download screen.dart and main_page_screen.dart created

---
+++ lib/presentation/main_page/widgets/main_page_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainPageScreen extends StatelessWidget {
+  const MainPageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
